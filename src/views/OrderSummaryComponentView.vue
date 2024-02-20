31726bcb1d303b05676cf37f3f9fd421f5ebd7a9
Oleksandra (SASHA) Honchar <oleksandra.honchar@on-running.com>
2024-02-20 17:33:52 +0100

    add stylevariables

--- src/views/OrderSummaryComponentView.vue
+++ src/views/OrderSummaryComponentView.vue
@@ -10,6 +10,26 @@
 </template>
 
 <style scoped lang="scss">
+/* colors: primary */
+$pale-blue: hsl(225, 100%, 94%);
+$bright-blue: hsl(245, 75%, 52%);
+
+/* colors: neutral */
+
+$very-pale-blue: hsl(225, 100%, 98%);
+$pale-blue-shadow: hsl(225, 49%, 88%);
+$desaturated-blue: hsl(224, 23%, 55%);
+$light-blue: hsl(245, 83%, 63%);
+$very-light-blue: hsl(245, 78%, 68%);
+$dark-blue: hsl(223, 47%, 23%);
+/* typography */
+$font-size-normal: 1rem;
+$font-size-small: 0.8rem;
+$font-size-very-small: 0.7rem;
+$font-name: 'Red Hat Display';
+$font-w-normal: 500;
+$font-w-bold: 700;
+$font-w-extrabold: 900;
 .main-container {
   width: 100%;
 }
