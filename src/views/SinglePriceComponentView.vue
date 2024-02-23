86630341d1c0faff3f097b8dc52b465605623bda
Oleksandra (SASHA) Honchar <oleksandra.honchar@on-running.com>
2024-02-23 09:27:02 +0100

    do basic html layout

--- src/views/SinglePriceComponentView.vue
+++ src/views/SinglePriceComponentView.vue
@@ -5,43 +5,42 @@ import MyDescription from '@/components/general/MyDescription.vue'
 <template>
   <div class="main-container">
     <MyDescription :routeName="`single-price-component`" />
-    <div class="component-container">
-      <section class="component-container">
-        <div class="grid">
-          <div class="grid__subgrid grid__subgrid--info">
-            <h2 class="grid__title grid__title--primary">Join our community</h2>
-            <h3 class="grid__subtitle">30-day, hassle-free money back guarantee</h3>
-            <span class="grid__description--primary">
-              Gain access to our full library of tutorials along with expert code reviews. Perfect
-              for any developers who are serious about honing their skills.
-            </span>
-          </div>
 
-          <div class="grid__subgrid grid__subgrid--subscription">
-            <h2 class="grid__title grid__title--secondary">Monthly Subscription</h2>
-            <div class="grid__subcontainer">
-              <p class="grid__description--price">&dollar;29</p>
-              <span class="grid__description--period">per month</span>
-            </div>
-            <p class="grid__description--secondary">2 Full access for less than &dollar;1 a day</p>
-            <button class="grid__button">Sign Up</button>
-          </div>
-
-          <div class="grid__subgrid grid__subgrid--about">
-            <h2 class="grid__title grid__title--secondary">Why Us</h2>
-            <ul class="grid__list">
-              <li class="grid__list-item">Tutorials by industry experts</li>
-              <li class="grid__list-item">Peer &amp; expert code review</li>
-              <li class="grid__list-item">Coding exercises</li>
-              <li class="grid__list-item">Access to our GitHub repos</li>
-              <li class="grid__list-item">Community forum</li>
-              <li class="grid__list-item">Flashcard decks</li>
-              <li class="grid__list-item">New videos every week</li>
-            </ul>
-          </div>
+    <section class="component-container">
+      <div class="grid">
+        <div class="grid__subgrid grid__subgrid--info">
+          <h2 class="grid__title grid__title--primary">Join our community</h2>
+          <h3 class="grid__subtitle">30-day, hassle-free money back guarantee</h3>
+          <span class="grid__description--primary">
+            Gain access to our full library of tutorials along with expert code reviews. Perfect for
+            any developers who are serious about honing their skills.
+          </span>
         </div>
-      </section>
-    </div>
+
+        <div class="grid__subgrid grid__subgrid--subscription">
+          <h2 class="grid__title grid__title--secondary">Monthly Subscription</h2>
+          <div class="grid__subcontainer">
+            <p class="grid__description--price">&dollar;29</p>
+            <span class="grid__description--period">per month</span>
+          </div>
+          <p class="grid__description--secondary">2 Full access for less than &dollar;1 a day</p>
+          <button class="grid__button">Sign Up</button>
+        </div>
+
+        <div class="grid__subgrid grid__subgrid--about">
+          <h2 class="grid__title grid__title--secondary">Why Us</h2>
+          <ul class="grid__list">
+            <li class="grid__list-item">Tutorials by industry experts</li>
+            <li class="grid__list-item">Peer &amp; expert code review</li>
+            <li class="grid__list-item">Coding exercises</li>
+            <li class="grid__list-item">Access to our GitHub repos</li>
+            <li class="grid__list-item">Community forum</li>
+            <li class="grid__list-item">Flashcard decks</li>
+            <li class="grid__list-item">New videos every week</li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
--- src/views/SocialProofComponentView.vue
+++ src/views/SocialProofComponentView.vue
@@ -5,7 +5,30 @@ import MyDescription from '@/components/general/MyDescription.vue'
 <template>
   <div class="main-container">
     <MyDescription :routeName="`social-proof-component`" />
-    <section class="component-container">Social Proof</section>
+    <section class="component-container">
+      <div class="social__top-container">
+        <div class="social__top-left-container">
+          <h1 class="social__title">10,000+ of our users love our products.</h1>
+          <p class="social__description">
+            We only provide great products combined with excellent customer service. See what our
+            satisfied customers are saying about our services.
+          </p>
+        </div>
+        <div class="social__rating-container">
+          Rated 5 Stars in Reviews Rated 5 Stars in Report Guru Rated 5 Stars in BestTech
+        </div>
+      </div>
+
+      <div class="social__bottom-container">
+        Colton Smith Verified Buyer "We needed the same printed design as the one we had ordered a
+        week prior. Not only did they find the original order, but we also received it in time.
+        Excellent!" Irene Roberts Verified Buyer "Customer service is always excellent and very
+        quick turn around. Completely delighted with the simplicity of the purchase and the speed of
+        delivery." Anne Wallace Verified Buyer "Put an order with this company and can only praise
+        them for the very high standard. Will definitely use them again and recommend them to
+        everyone!"
+      </div>
+    </section>
   </div>
 </template>
 
