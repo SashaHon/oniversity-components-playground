f8a170d5dbb2d5be69b9afc0ac06c147213c93e4
Oleksandra (SASHA) Honchar <oleksandra.honchar@on-running.com>
2024-02-28 17:03:35 +0100

    style navigation component

--- src/components/ecommerceProductPageComponent/EcomHeader.vue
+++ src/components/ecommerceProductPageComponent/EcomHeader.vue
@@ -7,3 +7,11 @@ import EcomNavigation from './EcomNavigation.vue'
     <EcomNavigation />
   </header>
 </template>
+
+<style scoped lang="scss">
+@import '@/assets/_ecommerce-product-page-variables.scss';
+
+header {
+  border-bottom: $c-grayish-blue 1px solid;
+}
+</style>
